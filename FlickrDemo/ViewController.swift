ba7bb20a4c9371f92f232508f62e388f34ec777b
Kaan Baris BAYRAK <kaan.bayrak@ozu.edu.tr>
2023-02-27 11:27:38 +0100

    Initial Commit

---
+++ FlickrDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrDemo
+//
+//  Created by CH Kaan on 27/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
