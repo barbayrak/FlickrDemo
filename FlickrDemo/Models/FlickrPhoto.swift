96b54c27decc75fcec0dcde33338c297e68231d9
Kaan Baris BAYRAK <kaan.bayrak@ozu.edu.tr>
2023-03-02 22:49:01 +0100

    Diffable Data Source fix

--- FlickrDemo/Models/FlickrPhoto.swift
+++ FlickrDemo/Models/FlickrPhoto.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
-struct FlickrPhoto : Codable {
+struct FlickrPhoto : Codable,Hashable {
     let id : String
     let owner : String
     let secret : String
     let server : String
     let title : String
+    
+    
+    // TODO: Interesting bug occurs here if we use Diffable data source without UUID, which is not hashing the values into unique hashes
+    let uuid = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+    
+    public static func == (lhs: FlickrPhoto, rhs: FlickrPhoto) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
 }
